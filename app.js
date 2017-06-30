96f7418fd7e6f848dc911352da9d0127a081feef
Mark Nelson <m.nelson@accenture.com>
2017-06-30 10:53:19 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express ();
+
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req,res){
+    res.render("home");
+})
+
+app.get("/about", function(req,res){
+    res.render("about");
+})
+
+
+app.listen(3000, function () {
+  console.log('up on localhost:3000')
+})
